ae8be665ea9344d791f40ad0f309aff115c1647d
kim5303123 <kim5303123@gmail.com>
2025-01-07 16:53:20 +0900

    Initial commit

---
+++ src/main/java/learnbyteaching/guestbook/dao/GuestbookDao.java
@@ -0,0 +1,15 @@
+package learnbyteaching.guestbook.dao;
+
+import java.util.List;
+
+import learnbyteaching.guestbook.vo.GuestbookVo;
+
+public interface GuestbookDao {
+//	public List<EmailVo> getList();
+//	public boolean insert(EmailVo vo);
+//	public boolean delete(Long no);
+	
+	public List<GuestbookVo> getList();
+	public boolean insert(GuestbookVo vo);
+	public boolean delete(Long no);
+}
